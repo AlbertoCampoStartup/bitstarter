91e0d8cb3e50febd2ce09ecabdc5863c862615b2
Alberto Campo <AlbertoCampoStartup@outlook.es>
2013-07-05 09:27:38 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(fs.readFileSync(./index.html,'utf8'));
-  response.send(buffer);
+  response.send('Hello');
 });
 
 var port = process.env.PORT || 5000;
