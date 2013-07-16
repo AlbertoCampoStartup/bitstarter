a7d5d8d6b72cefd086808cf2a779794dd0011679
Alberto Campo <AlbertoCampoStartup@outlook.es>
2013-07-16 11:37:14 +0000

    HW3 changes

--- grader.js
+++ grader.js
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://fierce-reaches-1073.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,14 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var urlFileExists = function(url) {
+    var instr = rest.get(url).toString();
+
+    return instr;
+};
+
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,7 +75,8 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+        .option('-u, --url <url_file>', 'URL of the file', clone(urlFileExists), URL_DEFAULT)
+      .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
