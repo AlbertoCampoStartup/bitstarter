ac88a35e0ec4cca0cdebf5022f38a6f5b985598f
Alberto Campo <AlbertoCampoStartup@outlook.es>
2013-08-01 12:01:28 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
@@ -9,7 +10,7 @@ app.get('/', function(request, response) {
   response.send(buffer.toString('utf8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
